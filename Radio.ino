ef65c64b3bca41f4c465e75acba1ba48ef777ab4
qdzqm <34876848+qdzqm@users.noreply.github.com>
2025-02-13 15:24:29 +0800

    Update Radio.ino
    Modified the mode switching code

--- Radio.ino
+++ Radio.ino
@@ -13,21 +13,31 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define NUMFLAKES     10 // Number of snowflakes in the animation example
 
 int j=0, i=0, k=0;
-bool FM_AM = 0; //0 is FM, 1 is AM
+bool FM_AM = HIGH; //HIGH is FM, LOW is MW
+bool currentMode = LOW; //HIGH is FM, LOW is MW
 void setup() {
   Serial.begin(57600);
   display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
   display.clearDisplay();
   Wire.begin();
+  pinMode(10,INPUT);
+  FM_AM = digitalRead(10); //D10作为波段开关
+  if (FM_AM == HIGH){setFM();currentMode = HIGH;}
+  else {setAM();currentMode = LOW;}
 }
 
 void loop() {
-  k = analogRead(A0);
-  if (k>800)
+  FM_AM = digitalRead(10);
+
+  if (FM_AM == HIGH && currentMode == LOW){
     setFM();
-  else
+    currentMode = HIGH;
+  }
+  if (FM_AM == LOW && currentMode == HIGH){
     setAM();
-  i = analogRead(A1);
+    currentMode = LOW;
+  }
+  i = analogRead(A1); //A1连接到CH，旋转电位器会引起CH电压变化，进而导致频率变化
   if(i > j+1 || i < j-1){
     uint32_t freq;
     float freqfm;
@@ -37,7 +47,10 @@ void loop() {
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(30, 12);
     freqfm = (float)freq/1000;
-    display.println(freqfm);
+    if(FM_AM == HIGH)
+      display.println(freqfm);
+    else
+      display.println(freq);
     j=i;
     display.display();      // Show initial text
     Serial.print(millis()/1000);
@@ -60,21 +73,19 @@ void setFM(){
   WriteRegister(0x1F, 0xd3);//INT interrupt is active high, FM_SMUTE_START_RSSI=default, FM_SMUTE_SLOPE_RSSI=default
 }
 void setAM(){
-  //delay(10);WriteRegister(0x71, 0x80);//Disable ADC, all others set to default
-  delay(10);WriteRegister(0x04, 0x80);//Clock initialization completed.
-  delay(10);WriteRegister(0x51, 0x02);//Dial controlled channel increase/decrease
-  delay(10);WriteRegister(0x88, 0xc8);//Change band, AM mode, High edge frequency of FM band set to 108MHz
-  delay(10);WriteRegister(0x99, 0x0a);//MW最低频率522KHz
-  delay(10);WriteRegister(0x98, 0x02);//MW最低频率522KHz
-  delay(10);WriteRegister(0x8c, 0x06);//MW最高频率1620KHz
-  delay(10);WriteRegister(0x8d, 0x54);//MW最高频率1620KHz
-  delay(10);WriteRegister(0x9b, 0x4a);//通道间隔1620-522=1098
-  delay(10);WriteRegister(0x9a, 0x04);//通道间隔1620-522=1098
-  delay(10);WriteRegister(0x18, 0x59);//FM Band Space 100KHz, MW Band Space 9KHz
-  //delay(10);WriteRegister(0xa0, 0x17);//Channel guard range in dial mode (default)
-  delay(10);WriteRegister(0x71, 0x04);//Enable Channel ADC, Channel ADC Start
-  delay(10);WriteRegister(0x22, 0xd5);//Enable Tune Interrupt, INT is edge triggered, FM_SMUTE_START_SNR=default
-  delay(10);WriteRegister(0x1F, 0xd3);//INT interrupt is active high, FM_SMUTE_START_RSSI=default, FM_SMUTE_SLOPE_RSSI=default
+  WriteRegister(0x04, 0x80);//Clock initialization completed.
+  WriteRegister(0x51, 0x02);//Dial controlled channel increase/decrease
+  WriteRegister(0x88, 0xc8);//Change band, AM mode, High edge frequency of FM band set to 108MHz
+  WriteRegister(0x99, 0x0a);//MW最低频率522KHz
+  WriteRegister(0x98, 0x02);//MW最低频率522KHz
+  WriteRegister(0x8c, 0x06);//MW最高频率1620KHz
+  WriteRegister(0x8d, 0x54);//MW最高频率1620KHz
+  WriteRegister(0x9b, 0x4a);//通道间隔1620-522=1098
+  WriteRegister(0x9a, 0x04);//通道间隔1620-522=1098
+  WriteRegister(0x18, 0x59);//FM Band Space 100KHz, MW Band Space 9KHz
+  WriteRegister(0x71, 0x04);//Enable Channel ADC, Channel ADC Start
+  WriteRegister(0x22, 0xd5);//Enable Tune Interrupt, INT is edge triggered, FM_SMUTE_START_SNR=default
+  WriteRegister(0x1F, 0xd3);//INT interrupt is active high, FM_SMUTE_START_RSSI=default, FM_SMUTE_SLOPE_RSSI=default
 }
 void WriteRegister(uint8_t reg, uint8_t value) {
   Wire.beginTransmission(0x35);
