59d3ac2c287b494a4783b26b70e9e8f128d9b03b
qdzqm <34876848+qdzqm@users.noreply.github.com>
2025-02-13 20:29:04 +0800

    Update Radio.ino
    bug fixed

--- Radio.ino
+++ Radio.ino
@@ -14,43 +14,41 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 int j=0, i=0, k=0;
 bool FM_AM = HIGH; //HIGH is FM, LOW is MW
-bool currentMode = LOW; //HIGH is FM, LOW is MW
+bool currentMode = HIGH; //HIGH is FM, LOW is MW
+uint32_t freq=0,freqAM=0;
+float freqFM=0;
 void setup() {
   Serial.begin(57600);
   display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
   display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
   Wire.begin();
   pinMode(10,INPUT);
   FM_AM = digitalRead(10); //D10作为波段开关
-  if (FM_AM == HIGH){setFM();currentMode = HIGH;}
-  else {setAM();currentMode = LOW;}
+  setFM();
+  delay(10);
+  UpdateDisplay();
 }
-
 void loop() {
   FM_AM = digitalRead(10);
 
   if (FM_AM == HIGH && currentMode == LOW){
     setFM();
     currentMode = HIGH;
+    display.setTextSize(1);
   }
   if (FM_AM == LOW && currentMode == HIGH){
     setAM();
     currentMode = LOW;
+    display.setTextSize(1);
   }
   i = analogRead(A1); //A1连接到CH，旋转电位器会引起CH电压变化，进而导致频率变化
   if(i > j+1 || i < j-1){
-    uint32_t freq;
-    float freqfm;
+
     GetFrequency(&freq);
-    display.clearDisplay();
-    display.setTextSize(2); // Draw 2X-scale text
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(30, 12);
-    freqfm = (float)freq/1000;
-    if(FM_AM == HIGH)
-      display.println(freqfm);
-    else
-      display.println(freq);
+    freqFM = (float)freq/1000;
+    freqAM = freq/50;
+    UpdateDisplay();      
     j=i;
     display.display();      // Show initial text
     Serial.print(millis()/1000);
@@ -68,6 +66,7 @@ void setFM(){
   WriteRegister(0x89, 0x70);//最高频率是108MHz
   WriteRegister(0x99, 0xd6);//最低频率是87.5MHz
   WriteRegister(0x98, 0x06);//最低频率是87.5MHz
+  WriteRegister(0x18, 0x59);//FM Band Space 100KHz, MW Band Space 9KHz
   WriteRegister(0x71, 0x04);//Enable Channel ADC, Channel ADC Start
   WriteRegister(0x22, 0xd5);//Enable Tune Interrupt, INT is edge triggered, FM_SMUTE_START_SNR=default
   WriteRegister(0x1F, 0xd3);//INT interrupt is active high, FM_SMUTE_START_RSSI=default, FM_SMUTE_SLOPE_RSSI=default
@@ -75,14 +74,14 @@ void setFM(){
 void setAM(){
   WriteRegister(0x04, 0x80);//Clock initialization completed.
   WriteRegister(0x51, 0x02);//Dial controlled channel increase/decrease
+  WriteRegister(0x9b, 0x7a);//通道间隔(1620-522)/9=122
+  WriteRegister(0x9a, 0x00);//通道间隔(1620-522)/9=122
   WriteRegister(0x88, 0xc8);//Change band, AM mode, High edge frequency of FM band set to 108MHz
   WriteRegister(0x99, 0x0a);//MW最低频率522KHz
   WriteRegister(0x98, 0x02);//MW最低频率522KHz
   WriteRegister(0x8c, 0x06);//MW最高频率1620KHz
   WriteRegister(0x8d, 0x54);//MW最高频率1620KHz
-  WriteRegister(0x9b, 0x4a);//通道间隔1620-522=1098
-  WriteRegister(0x9a, 0x04);//通道间隔1620-522=1098
-  WriteRegister(0x18, 0x58);//FM Band Space 100KHz, MW Band Space 9KHz
+  WriteRegister(0x18, 0x59);//FM Band Space 100KHz, MW Band Space 9KHz
   WriteRegister(0x71, 0x04);//Enable Channel ADC, Channel ADC Start
   WriteRegister(0x22, 0xd5);//Enable Tune Interrupt, INT is edge triggered, FM_SMUTE_START_SNR=default
   WriteRegister(0x1F, 0xd3);//INT interrupt is active high, FM_SMUTE_START_RSSI=default, FM_SMUTE_SLOPE_RSSI=default
@@ -108,4 +107,26 @@ void GetFrequency(uint32_t *frequency) {
   ReadRegister(0xe5, &low_byte);
   uint32_t channel = (high_byte << 8) | low_byte;
   *frequency = channel * 50;
+}
+
+void UpdateDisplay(){
+  display.clearDisplay();
+  if(FM_AM == HIGH){
+    display.setCursor(20, 12);
+    display.setTextSize(2);
+    display.print(freqFM);
+    display.setTextSize(1);
+    display.print(" MHz");
+    display.setCursor(110, 22);
+    display.print("FM");
+  }
+  else{
+    display.setCursor(20, 12);
+    display.setTextSize(2);
+    display.print(freqAM);
+    display.setTextSize(1);
+    display.print(" KHz");
+    display.setCursor(110, 22);
+    display.print("MW");
+  }
 }
